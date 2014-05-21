47506fd6459d9d82f37c0284176717d00cad1ffd
taojonesin <taojonesin (at) nowhere.com>
2014-05-21 14:57:43 -0700

    passing numbers, booleans, null, and strings

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,14 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var objType = typeof obj;
+  console.log(objType);
+  console.log(obj);
+  if (objType === 'number' || objType === 'boolean' || obj === null) {
+    return String(obj);
+  } else if (objType === 'string' ) {
+    return String('"' + obj + '"');
+  } else if (Array.isArray(obj)) {
+    // Pass
+  }
 };
