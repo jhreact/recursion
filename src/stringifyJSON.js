1d6371deef9c7b49775f73b141eaa38168d98d92
taojonesin <taojonesin (at) nowhere.com>
2014-05-21 19:41:29 -0700

    passing flat array tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,13 +5,37 @@
 var stringifyJSON = function(obj) {
   // your code goes here
   var objType = typeof obj;
-  console.log(objType);
-  console.log(obj);
   if (objType === 'number' || objType === 'boolean' || obj === null) {
     return String(obj);
   } else if (objType === 'string' ) {
     return String('"' + obj + '"');
-  } else if (Array.isArray(obj)) {
+  } else if (objType === 'object') {
+    var result = [];
+    for (var prop in obj) {
+      var elemType = typeof obj[prop];
+      if (elemType !== 'object') {
+        result.push(stringifyJSON(obj[prop]));
+      } else {
+        // if (Array.isArray(obj)) {
+        //   result.push(stringifyJSON(obj[prop]));
+        // } else {
+        //   result.push('"' + String(prop) + '": ' + stringifyJSON(obj[prop]));
+        // }
+        result.push(obj[prop]);
+      }
+    }
+    var resConverted;
+    if (result) {
+      resConverted = result.join(',');
+    }
+    console.log(resConverted);
+    if (Array.isArray(obj)) {
+      return '[' + resConverted + ']';
+    } else {
+      return '{' + resConverted + '}';
+    }
     // Pass
+    // console.log(objType);
+    // console.log(obj);
   }
 };
