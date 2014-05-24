f997a322c6ae8566ce40f838c39cc1db193e883a
taojonesin <taojonesin (at) nowhere.com>
2014-05-24 11:21:03 -0700

    getElementsByClassName matches results of calling the built-in function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,32 +9,23 @@
 // element.childNodes : collection of child nodes for given element
 // element.classList : class attribute as a list of tokens
 var getElementsByClassName = function(className){
-  // your code here
-  //
-  // if match, push on results
-  // if childNodes
-  //    foreach cn getElementsByClassName
   var results = [];
   var args = [].slice.call(arguments);
-  var elem = args.length === 2 ? [args[1]] : window.document.body;
-  var cList = [].slice.call(elem.classList).toString();
-  console.log(cList);
-  if (cList.indexOf(className) !== -1) {
-    results.push(elem);
+  var elem = args.length === 2 ? args[1] : window.document.body;
+  if (elem.classList) {
+    var cList = [].slice.call(elem.classList).toString();
+    if (cList.indexOf(className) !== -1) {
+      results.push(elem);
+    }
   }
-  // var elemChildren = [].slice.call(elem.childNodes);
   var elemChildren = elem.childNodes;
-
   for (var i=0; i < elemChildren.length; i++) {
-    eChild = elemChildren[i];
-    // console.log("The fracking elem");
-
-    // if (elemChildren) {
-    //   elemChildren.forEach(function() {
-    //     getElementsByClassName(className);
-    //   });
-    // }
+    var eChild = elemChildren[i];
+    childMatches = getElementsByClassName(className, eChild);
+    childMatches.forEach(function(val, idx, arr) {
+      results.push(val);
+    });
   };
-  console.log(results);
+  //console.log(results);
   return results;
 };
